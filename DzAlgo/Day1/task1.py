7947e20402b154ec25bfe2f677438b0228fe3842
Andrey <Andrei1592a@mail.ru>
2020-11-10 16:02:13 +0300

    test

--- DzAlgo/Day1/task1.py
+++ DzAlgo/Day1/task1.py
@@ -8,7 +8,7 @@ numberSumm = a + b + c
 summs = a * b * c
 print(f"Сумма цифр вашего трехзначного числа = {numberSumm}")
 print(f"Произведение цифр вашего трехзначного числа = {summs}")
-
+1
 
 # другое решение
 sum = str(number)
